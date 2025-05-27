e6e9708fbd9d21e43d2d8dd0ce444d6dcca2e2f9
Solijon <ssolijon94@gmail.com>
2025-05-27 14:44:56 +0500

    27.06.2025

---
+++ app/src/main/java/com/example/weatherapp/fragments/SettingsFragment.kt
@@ -0,0 +1,34 @@
+package com.example.weatherapp.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentSettingsBinding
+
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private var _binding : FragmentSettingsBinding? = null
+    private val  binding get() = _binding!!
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSettingsBinding.bind(view)
+
+
+        binding.settingsBackButton.setOnClickListener {
+            findNavController().navigate(R.id.action_settingsFragment_to_currentWeatherFragment)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+
+    }
+
+}
